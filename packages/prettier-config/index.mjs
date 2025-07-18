d6a5b494d8c752198ff2a8781f52f74812485bb4
Gabin Aureche <hello@gabinaureche.com>
2025-07-18 11:51:21 +0200

    Remove unnecessary prettier config override

--- packages/prettier-config/index.mjs
+++ packages/prettier-config/index.mjs
@@ -24,7 +24,6 @@ const config = {
     "^[./]",
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
 };
 
 export default config;
