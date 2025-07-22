ec0981c3c85e1ea512ae2138827a74e2cd11701d
Gabin Aureche <hello@gabinaureche.com>
2025-07-22 17:25:07 +0200

    Raise typescript error in browser

--- apps/simulateurs/vite.config.ts
+++ apps/simulateurs/vite.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
+    checker({ typescript: true }),
     tsConfigPaths(),
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
