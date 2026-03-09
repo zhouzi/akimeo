62559674f50cceb5445636a31367c6fd0ea66a02
Gabin Aureche <hello@gabinaureche.com>
2026-03-09 17:02:01 +0400

    Add more salarie helpers

--- packages/modele/src/poste/helpers.ts
+++ packages/modele/src/poste/helpers.ts
@@ -1,9 +1,9 @@
 import type { PartialDeep } from "type-fest";
 import defaultsDeep from "lodash.defaultsdeep";
 
-import type { Salarie } from "./types";
+import type { AnyPoste, Apprenti, Salarie, Stagiaire } from "./types";
 import { CONTRAT_POSTE } from "./constants";
-import { salarieSchema } from "./schemas";
+import { apprentiSchema, salarieSchema, stagiaireSchema } from "./schemas";
 
 export function creerSalarie(salarie: PartialDeep<Salarie>): Salarie {
   return salarieSchema.parse(
@@ -11,4 +11,36 @@ export function creerSalarie(salarie: PartialDeep<Salarie>): Salarie {
       contrat: CONTRAT_POSTE.cdi.value,
     } satisfies Salarie),
   );
+}
+
+export function creerApprenti(apprenti: PartialDeep<Apprenti>): Apprenti {
+  return apprentiSchema.parse(
+    defaultsDeep({}, apprenti, {
+      contrat: CONTRAT_POSTE.apprentissage.value,
+    } satisfies Apprenti),
+  );
+}
+
+export function creerStagiaire(stagiaire: PartialDeep<Stagiaire>): Stagiaire {
+  return stagiaireSchema.parse(
+    defaultsDeep({}, stagiaire, {
+      contrat: CONTRAT_POSTE.stage.value,
+    } satisfies Stagiaire),
+  );
+}
+
+export function creerAnyPoste<T extends AnyPoste["contrat"]>(
+  anyPoste: { contrat: T } & PartialDeep<Extract<AnyPoste, { contrat: T }>>,
+) {
+  switch (anyPoste.contrat) {
+    case CONTRAT_POSTE.apprentissage.value:
+    case CONTRAT_POSTE.professionnalisation.value:
+      return creerApprenti(anyPoste as PartialDeep<Apprenti>);
+    case CONTRAT_POSTE.stage.value:
+      return creerStagiaire(anyPoste as PartialDeep<Stagiaire>);
+    case CONTRAT_POSTE.cdi.value:
+    case CONTRAT_POSTE.cdd.value:
+    default:
+      return creerSalarie(anyPoste as PartialDeep<Salarie>);
+  }
 }
