b5b172ec1b5921595a5f4b911d6c1e3f78064bc4
Gabin Aureche <hello@gabinaureche.com>
2026-04-26 10:32:18 +0200

    Add EI helpers

--- packages/modele/src/entreprise/helpers.ts
+++ packages/modele/src/entreprise/helpers.ts
@@ -1,9 +1,14 @@
 import type { PartialDeep } from "type-fest";
 import defaultsDeep from "lodash.defaultsdeep";
 
-import type { MicroEntreprise, Sarl, SAS } from "./types";
+import type { EI, MicroEntreprise, Sarl, SAS } from "./types";
 import { NATURE_ACTIVITE_ENTREPRISE, STATUT_ENTREPRISE } from "./constants";
-import { microEntrepriseSchema, sarlSchema, sasSchema } from "./schemas";
+import {
+  eiSchema,
+  microEntrepriseSchema,
+  sarlSchema,
+  sasSchema,
+} from "./schemas";
 
 export function creerMicroEntreprise(
   microEntreprise: PartialDeep<MicroEntreprise>,
@@ -16,6 +21,17 @@ export function creerMicroEntreprise(
       tva: false,
       versementLiberatoire: false,
     } satisfies MicroEntreprise),
+  );
+}
+
+export function creerEI(ei: PartialDeep<EI>): EI {
+  return eiSchema.parse(
+    defaultsDeep({}, ei, {
+      statut: STATUT_ENTREPRISE.ei.value,
+      natureActivite: NATURE_ACTIVITE_ENTREPRISE.liberale.value,
+      acre: false,
+      tva: true,
+    } satisfies EI),
   );
 }
 
